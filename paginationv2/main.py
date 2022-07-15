8acfe0ea3edecf6cba0a82bcf54b7b3f4f3a67a3
AS1 <arseniykofanov@gmail.com>
2022-07-15 11:38:43 +0300

    Update main.py

--- paginationv2/main.py
+++ paginationv2/main.py
@@ -6,7 +6,7 @@ bot = telebot.TeleBot(config.TOKEN) # bot from TeleBot class
 #handlers
 @bot.message_handler(commands=['start'])
 def welcomer(message):
-    with open("photos/1.jpg","rb") as photo_file: # Open file-photo
+    with open(f"photos/1{config.prefix}","rb") as photo_file: # Open file-photo
         main = types.InlineKeyboardMarkup() # Button object
         main.add(types.InlineKeyboardButton('⏮',callback_data=f"-1|1|{config.prefix}"),types.InlineKeyboardButton('⏭',callback_data=f"+1|1|{config.prefix}")) # Markups
         bot.send_photo(message.chat.id,photo_file,caption="Choose:",reply_markup=main)
@@ -19,9 +19,9 @@ def call_handler_processer(call):
     if num > config.max_photos:
         num = 1
     if num == 0:
-        num = 5
+        num = config.max_photos
     main.add(types.InlineKeyboardButton('⏮',callback_data=f"-1|{str(num)}|{config.prefix}"),types.InlineKeyboardButton('⏭',callback_data=f"+1|{str(num)}|{config.prefix}")) # Markups
-    get_photo_location = "photos/" + str(num) + ".jpg"
+    get_photo_location = "photos/" + str(num) + config.prefix
     with open(get_photo_location,"rb") as updated_file:
         bot.edit_message_media(media=types.InputMedia(type='photo', media=updated_file),chat_id=call.message.chat.id,message_id=call.message.message_id,reply_markup=main)
 
